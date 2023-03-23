c9fab9d87cbb46a398a410db779cea73561fca66
mddhafa <mdmuhammaddhafa1234@gmail.com>
2023-03-24 02:55:22 +0700

    menambahkan void input

--- insertion algoritm/insertion algoritm.cpp
+++ insertion algoritm/insertion algoritm.cpp
@@ -3,3 +3,18 @@ using namespace std;
 
 int arr[20];
 int n;
+
+void input() {
+	while (true) {
+		cout << "masukkan jumlah data pada array : ";
+		cin >> n;
+
+		if (n <= 20) {
+			break;
+		}
+		else
+		{
+			cout << "\n array yang anda masukkan maksimal 20 elemen. \n";
+		}
+	}
+}
